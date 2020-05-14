28937de5d14da290f3404dc35321433e858b7134
Yaroslav <bys.99@mail.ru>
2020-05-14 11:25:36 +0300

    Create DateManipulator.java
    Add DateManipulator with calendar

---
+++ src/factoryexample/DateManipulator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factoryexample;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author bys99
+ */
+public class DateManipulator {
+    Calendar calendar = new GregorianCalendar(2020, Calendar.APRIL, 1);
+    Calendar today = new GregorianCalendar();
+    
+    public void nextDay() {
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+    }
+    
+    public int getDate() {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+    
+    public boolean DateIsOver() {
+        return calendar.after(today);
+    }
+}
