5994ef962dc6968f2d41d7721d816281ed38442c
Yaroslav <bys.99@mail.ru>
2020-05-14 14:00:34 +0300

    Update DateManipulator.java
    Change getNextMonth

--- src/factoryexample/DateManipulator.java
+++ src/factoryexample/DateManipulator.java
@@ -21,8 +21,12 @@ public class DateManipulator {
         calendar.add(Calendar.DAY_OF_MONTH, 1);
     }
     
-    public void nextMonth() {
-        calendar.add(Calendar.MONTH, 1);
+    public Calendar getNextMonth() {
+        Calendar calendar1 = new GregorianCalendar();
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        calendar1 = calendar;
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        return calendar1;
     }
     
     public Date getDate() {
